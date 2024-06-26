3d340050c6da9ae1b4df1db2f13e18f99e1a9925
Ryelics <anuphat880@hotmail.com>
2024-06-26 15:53:12 +0700

    Q3.6

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ createApp ({
         const product = ref('boot')
         const image = ref('./assets/images/socks_green.jpg')
         const description = ref('this is boots use to walk')
+        const click = ref('https://www.camt.cmu.ac.th/index.php/th/')
         const inStock = ref(true)
+    
         return{
             product,
             inStock,
             description,
-            image
+            image,
+            click
         }
     }
 

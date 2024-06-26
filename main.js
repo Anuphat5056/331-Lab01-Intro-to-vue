b75ae05793a42d312b6b73cb53fb13ad802f22d1
Ryelics <anuphat880@hotmail.com>
2024-06-26 15:31:38 +0700

    Q3.2

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const {createApp, ref} = Vue;
 createApp ({
     setup() {
         const product = ref('boot')
+        const image = ref('./assets/images/sock_green.jpg')
         const description = ref('this is boots use to walk')
         const inStock = ref(true)
         return{
             product,
             inStock,
-            description
+            description,
+            image
         }
     }
 

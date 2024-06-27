234eb823cfc17eb30334ef47d4b8fa39c4c3f3ce
Ryelics <anuphat880@hotmail.com>
2024-06-27 22:01:09 +0700

    Q11.2(2)

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -23,6 +23,7 @@ const productDisplay = {
             <button class="button" :disable='!inStock' @click="addToCart" :class="{'disabledButton': !inStock}">Add To Cart</button>
             <button class="button" :disable='!inStock' @click="removeFormCart" :class="{'disabledButton': !inStock}">Remoce from cart</button>
             <button class="button" @click="changeStatus">{{ inStock ? 'Out of Stock' : 'In Stock' }}</button>
+            <review-from></reviewform>
         </div>`,
         props: {
             premium: Boolean,

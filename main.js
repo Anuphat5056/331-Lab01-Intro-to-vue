194dab7f2c617122dd3584469e990d6536721240
Ryelics <anuphat880@hotmail.com>
2024-06-27 21:23:59 +0700

    Q10.4

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const {createApp, ref, computed} = Vue;
 
 const app = createApp ({
     setup() {
-        const cart = ref(0)
+        const cart = ref([])
         const premium = ref(true)
-        function updateCart() {
-            cart.value +=1
+        function updateCart(id) {
+            cart.value.push(id)
         }
         const details = ref(["50% cotton", "30% wool", "20% polyester"])
         return{
